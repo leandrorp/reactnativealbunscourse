44c729cac6f5a38b0f33ffa06108e7a04428f1a7
Leandro Rezende Pinheiro <lrezendepinheiro@gmail.com>
2019-01-29 11:02:54 -0200

    Publishing the project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { View , Text } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex : 1}}>
+        <Header headerText={'Album'}/>
+        <AlbumList />
+      </View>
+    );
+  }
+}
+
